d514ddfa67d6cb5afefa680b9617af50af2ee566
Yingchao Ji <yingchaojistone@163.com>
2024-02-06 05:12:13 +0100

    Implement decode.c

--- WP2: C_Programming/code.c
+++ WP2: C_Programming/code.c
@@ -3,53 +3,50 @@
 // Exercise 4
 // Submission code: xxxxxx (provided by your TA-s)
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> // Declare standard IO header
+#include <stdlib.h> // Declare standard library header
 
-#define binary [9]
+int main(int argc, char *argv[]) { // Main function with arguments
 
-int main(int argc, char *argv[]) {
-
-  if (argc == 6) {
-    int arg1 = atoi(argv[1]);
-    int arg2 = atoi(argv[2]);
-    int arg3 = atoi(argv[3]);
-    int arg4 = atoi(argv[4]);
-    int arg5 = atoi(argv[5]);
-
-    if ((arg1 < 0 || arg1 > 1) || (arg2 < 0 || arg2 > 4) ||
-        (arg3 < 0 || arg3 > 2) || (arg4 < 0 || arg4 > 1) ||
-        (arg5 < 0 || arg5 > 1)) {
-      printf("Invalid argument range.");
-      printf("Usage: ./code <0..1>  <0..4> <0..2> <0..1> <0..1>. \n");
-      exit(1);
-    } else {
-
-      int binNum = 0;
-
-      binNum = (binNum << 1) | arg1;
-
-      for (int i = 2; i >= 0; i--) {
-        int bit = (arg2 >> i) & 1;
-        binNum = (binNum << 1) | bit;
-      }
-
-      for (int i = 1; i >= 0; i--) {
-        int bit = (arg3 >> i) & 1;
-        binNum = (binNum << 1) | bit;
-      }
-
-      binNum = (binNum << 1) | arg4;
-      binNum = (binNum << 1) | arg5;
-
-      printf("%X", binNum);
+    if (argc != 6) { // If the number of arguments is not 6
+        printf("Arguments number error. \n"); // Print the error prompt
+        printf("Usage: ./code <number>  <number> <number> <number> <number>. \n"); // Print the Usage
+        return 1; // End program unsuccessful
     }
 
-  } else {
-    printf("Arguments number error. \n");
-    printf("Usage: ./code <number>  <number> <number> <number> <number>. \n");
-    return 1;
-  }
+    int arg1 = atoi(argv[1]); // Convert argument 1 to integer
+    int arg2 = atoi(argv[2]); // Convert argument 2 to integer
+    int arg3 = atoi(argv[3]); // Convert argument 3 to integer
+    int arg4 = atoi(argv[4]); // Convert argument 4 to integer
+    int arg5 = atoi(argv[5]); // Convert argument 5 to integer
 
-  return 0;
+    if ((arg1 < 0 || arg1 > 1) || (arg2 < 0 || arg2 > 4) || // If argument 1 is between 0 and 1, or argument 2 is between 0 and 4, or
+        (arg3 < 0 || arg3 > 2) || (arg4 < 0 || arg4 > 1) || // If argument 3 is between 0 and 2, or argument 4 is between 0 and 1, or
+        (arg5 < 0 || arg5 > 1)) { // If argument 5 is between 0 and 1
+        printf("Invalid argument range."); // Print invalid prompt
+        printf("Usage: ./code <0..1>  <0..4> <0..2> <0..1> <0..1>. \n"); // Print Usage
+        return 1; // End program unsuccessful
+    }
+
+    int binNum = 0; // Initialize binNum is 0
+
+    // Using bit operation to convert each arguments to binary
+    binNum = (binNum << 1) | arg1; // Convert argument 1 to binary. 1 bit 
+
+    for (int i = 2; i >= 0; i--) { // Using for loop convert each bit of argument 2 to binary. 3 bits
+        int bit = (arg2 >> i) & 1; // Extract each bit of argument 2
+        binNum = (binNum << 1) | bit; // Append the bit to binNum
+    }
+
+    for (int i = 1; i >= 0; i--) { // Using for loop convert each bit of argument 3 to binary. 2 bits
+        int bit = (arg3 >> i) & 1; // Extract each bit of argument 3
+        binNum = (binNum << 1) | bit; // Append the bit to binNum
+    }
+
+    binNum = (binNum << 1) | arg4; // Convert argument 4 to binary. 1 bit 
+    binNum = (binNum << 1) | arg5; // Convert argument 5 to binary. 1 bit 
+
+    printf("%X", binNum); // Print the number in hexadecimal format
+
+    return 0; // End program successful
 }
--- WP2: C_Programming/decode.c
+++ WP2: C_Programming/decode.c
@@ -3,3 +3,54 @@
 // Exercise 4
 // Submission code: xxxxxx (provided by your TA-s)
 
+#include <stdio.h> // Declare standard IO header
+#include <string.h> // Declare string library header
+
+int main(int argc, char *argv[]) { // Main function with arguments
+
+    if (argc != 2) { // If the number of arguments is not 2
+        printf("Arguments number error. \n"); // Print the error prompt
+        printf("Usage: ./decode <hexadecimal number>. \n"); // Print the Usage
+        return 1; // End program unsuccessful
+    } 
+
+    int decNum = 0; // Initialize decNum is 0
+    char *hexaNum = argv[1]; // Initialize hexaNum point argv[1]
+    for (int i = strlen(hexaNum) - 1; i >= 0; i --) { // Iterater each charater of argument
+        char currentChar = hexaNum[i]; // Initialize currentChar point hexaNum[i]
+        int value; // Declare value to calculate the value of each bit 
+        if (currentChar >= 'A' && currentChar <= 'F'){ // If the character is between A to F
+            value = 10 + currentChar - 'A'; // Calculate the value by - 'A'
+        }else if(currentChar >= '0' && currentChar <= '9'){ // If the character is between 0 to 10
+            value = currentChar - '0'; // Calculate the value by - '0'
+        } else { // When the charater is not a hexadecimal 
+            printf("Invalid argument."); // Print invalid prompt
+            printf("Usage: ./decode <hexadecimal number>. \n"); // Print the Usage
+            return 1; // End program unsuccessful
+        }
+
+        int weight = 1; // Initialize a weight 1. When bit is 0 the weight is 1 
+        for (int j = strlen(hexaNum) - 1; j > i; j --) { // Using for loop to calculate the weight. one j represent one power
+            weight *= 16; // 16^n   n is round of loop
+        }
+
+        decNum += weight * value; // Calculate the decimal number by add each weight of bit
+    }
+
+    int dec1 = (decNum >> 7) & 1; // Using bit operation to get the value of bit 7, one bit
+    int dec2 = (decNum >> 4) & 7; // Using bit operation to get the value of bit 4, 5, 6, three bits. 7 is the biggest number of three bit
+    int dec3 = (decNum >> 2) & 3; // Using bit operation to get the value of bit 2, 3, two bits. 3 is the biggest number of two bit 
+    int dec4 = (decNum >> 1) & 1; // Using bit operation to get the value of bit 1, one bit
+    int dec5 = decNum & 1; // Using bit operation to get the value of bit 0, one bit
+
+    // Print the result as a table
+    printf("Name            Value\n");
+    printf("-----------------------------\n");
+    printf("engine_on         %d\n", dec1);
+    printf("gear_pos          %d\n", dec2);
+    printf("key_pos           %d\n", dec3);
+    printf("brake1            %d\n", dec4);
+    printf("brake2            %d\n", dec5);
+
+    return 0; // End program successful
+}
